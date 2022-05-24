4f64b8877d1af223fc6448c9deb3ceb3156d4a4b
Mehmet Ozen <ozenmehmet.92@gmail.com>
2022-05-24 10:14:20 +0300

    Added anorher IsPrime function

--- Program.cs
+++ Program.cs
@@ -22,12 +22,27 @@ namespace Prime_Number_Finder
 
         }
 
-        static bool IsPrime(int n)
-        {
-            if (n > 1)
-                return Enumerable.Range(1, n).Where(x => n % x == 0).SequenceEqual(new[] { 1, n });
+        //static bool IsPrime(int n)
+        //{
+        //    if (n > 1)
+        //        return Enumerable.Range(1, n).Where(x => n % x == 0).SequenceEqual(new[] { 1, n });
 
-            return false;
+        //    return false;
+        //}
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
         }
     }
 }
